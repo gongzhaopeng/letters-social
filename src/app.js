bf24415c86f2e8fd202a82702f2516ee77931e55
Zhaopeng Gong <zhaopeng@ZhaopengdeiMac.local>
2019-06-12 19:36:23 +0800

    ...

--- src/app.js
+++ src/app.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import parseLinkHeader from 'parse-link-header';
-import orderBy from 'lodash/orderBy';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import parseLinkHeader from "parse-link-header";
+import orderBy from "lodash/orderBy";
 
-import ErrorMessage from './components/error/Error';
-import Nav from './components/nav/navbar';
-import Loader from './components/Loader';
+import Nav from "./components/nav/navbar";
+import Loader from "./components/Loader";
+import * as API from "./shared/http";
 
-import * as API from './shared/http';
-import Ad from './components/ad/Ad';
-import Post from './components/post/Post';
-import Welcome from './components/welcome/Welcome';
+import Ad from "./components/ad/Ad";
+import Welcome from "./components/welcome/Welcome";
 
 /**
  * The app component serves as a root for the project and renders either children,
@@ -26,64 +24,58 @@ class App extends Component {
             loading: false,
             posts: [],
             endpoint: `${process.env
-                .ENDPOINT}/posts?_page=1&_sort=date&_order=DESC&_embed=comments&_expand=user&_embed=likes`,
+                .ENDPOINT}/posts?_page=1&_sort=date&_order=DESC&_embed=comments&_expand=user&_embed=likes`
         };
         this.getPosts = this.getPosts.bind(this);
     }
-    static propTypes = {
-        children: PropTypes.node,
-    };
+
     componentDidMount() {
         this.getPosts();
     }
-    componentDidCatch(err, info) {
-        console.error(err);
-        console.error(info);
+
+    componentDidCatch(error, errorInfo) {
+        console.error(error);
+        console.error(errorInfo);
         this.setState(() => ({
-            error: err,
+            error
         }));
     }
+
+    static propTypes = {
+        children: PropTypes.node
+    };
+
     getPosts() {
         API.fetchPosts(this.state.endpoint)
             .then(res => {
-                return res.json().then(posts => {
-                    const links = parseLinkHeader(res.headers.get('Link'));
-                    this.setState(() => ({
-                        posts: orderBy(this.state.posts.concat(posts), 'date', 'desc'),
-                        endpoint: links.next.url,
-                    }));
-                });
-            })
-            .catch(err => {
-                this.setState(() => ({ error: err }));
+                return res
+                    .json()
+                    .then(posts => {
+                        const links = parseLinkHeader(res.headers.get("Link"));
+                        this.setState(() => ({
+                            posts: orderBy(this.state.posts.concat(posts),
+                                "date", "desc"),
+                            endpoint: links.next.url
+                        }));
+                    })
+                    .catch(err => {
+                        this.setState(() => ({ error: err }));
+                    });
             });
     }
+
     render() {
-        if (this.state.error) {
-            return (
-                <div className="app">
-                    <ErrorMessage error={this.state.error} />
-                </div>
-            );
-        }
         return (
             <div className="app">
-                <Nav user={this.props.user} />
+                <Nav/>
                 {this.state.loading ? (
                     <div className="loading">
-                        <Loader />
+                        <Loader/>
                     </div>
                 ) : (
                     <div className="home">
-                        <Welcome key="welcome" />
+                        <Welcome key="welcome"/>
                         <div>
-                            {this.state.posts.length && (
-                                <div className="posts">
-                                    {this.state.posts.map(({ id }) => {
-                                        return <Post id={id} key={id} user={this.props.user} />;
-                                    })}
-                                </div>
-                            )}
                             <button className="block" onClick={this.getPosts}>
                                 Load more posts
                             </button>
