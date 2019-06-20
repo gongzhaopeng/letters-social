146c6ec6ebf9b34467242f84c83466c59733fa61
gong zhaopeng <gongzhaopeng@gongs-MacBook-Air.local>
2019-06-20 23:08:14 +0800

    ...

---
+++ src/components/map/DisplayMap.js
@@ -0,0 +1,43 @@
+/**
+ * Created by gongzhaopeng on 2019/6/20.
+ */
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+export default class DisplayMap extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mapLoaded: false,
+            location: {
+                lat: props.location.lat,
+                lng: props.location.lng,
+                name: props.location.name
+            }
+        };
+        this.ensureMapExists = this.ensureMapExists.bind(this);
+    }
+
+    static propTypes = {
+        location: PropTypes.shape({
+            lat: PropTypes.number,
+            lng: PropTypes.number,
+            name: PropTypes.string
+        }),
+        displayOnly: PropTypes.bool
+    };
+
+    render() {
+        return [
+            <div key="displayMap" className="displayMap">
+                <div
+                    className="map"
+                    ref={node => {
+                        this.mapNode = node;
+                    }}
+                >
+                </div>
+            </div>
+        ];
+    }
+}
