c16157617c59af47da11ce37c54d8c76db7886bd
Zhaopeng Gong <zhaopeng@ZhaopengdeiMac.local>
2019-09-12 14:53:55 +0800

    ...

--- src/components/router/Router.js
+++ src/components/router/Router.js
@@ -6,7 +6,7 @@ import invariant from "invariant";
 
 export default class Router extends Component {
     static propTypes = {
-        children: PropTypes.object,
+        children: PropTypes.array,
         location: PropTypes.string.isRequired
     };
 
@@ -15,22 +15,49 @@ export default class Router extends Component {
 
         this.routes = {};
 
-        // TODO
+        this.addRoutes(props.children);
+
+        this.router = enroute(this.routes);
     }
 
     addRoute(element, parent) {
-        // TODO
+        const { component, path, children } = element.props;
+
+        invariant(component, `Route ${path} is missing the "component" property`);
+        invariant(typeof path === "string", `Route ${path} is not a string`);
+
+        const render = (params, renderProps) => {
+            const finalProps = Object.assign(
+                { params },
+                this.props,
+                renderProps);
+
+            const children = React.createElement(component, finalProps);
+
+            return parent ? parent.render(params, { children }) : children;
+        };
+
+        const route = Router.normalizeRoute(path, parent);
+
+        if (children) {
+            this.addRoutes(children, { route, render });
+        }
+
+        this.routes[Router.cleanPath(route)] = render;
     }
 
     addRoutes(routes, parent) {
-        React.children.forEach(routes);
+        React.children.forEach(
+            routes,
+            route => this.addRoute(route, parent)
+        );
     }
 
-    cleanPath(path) {
+    static cleanPath(path) {
         return path.replace(/\/\//g, "/");
     }
 
-    normalizeRoute(path, parent) {
+    static normalizeRoute(path, parent) {
         if (path[0] === "/") {
             return path;
         }
@@ -41,6 +68,8 @@ export default class Router extends Component {
     }
 
     render() {
-        // TODO
+        const { location } = this.props;
+        invariant(location, "<Router/> needs a location to work");
+        return this.router(location);
     }
 }
