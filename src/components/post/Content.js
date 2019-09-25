cb39cc68e8e6ab73a210e4ee1479c17a7f525bfb
Zhaopeng Gong <zhaopeng@ZhaopengdeiMac.local>
2019-09-25 14:55:39 +0800

    ...

--- src/components/post/Content.js
+++ src/components/post/Content.js
@@ -1,5 +1,5 @@
-import PropTypes from 'prop-types';
-import React from 'react';
+import PropTypes from "prop-types";
+import React from "react";
 
 const Content = ({ post }) => <p className="content">{post.content}</p>;
 
--- src/components/post/Content.test.js
+++ src/components/post/Content.test.js
@@ -1,7 +1,28 @@
+import React from "react";
+import { shallow } from "enzyme";
+import renderer from "react-test-renderer";
 import Content from "./Content";
 
 describe("<Content/>", () => {
-    test("should render correctly", () => {
-
+    describe("render methods", () => {
+        it("should render correctly", () => {
+            const mockPost = {
+                content: "I am learning to test React components"
+            };
+            const wrapper = shallow(<Content post={mockPost}/>);
+            expect(wrapper.find("p").length).toBe(1);
+            expect(wrapper.find("p.content").length).toBe(1);
+            expect(wrapper.find(".content").text()).toBe(mockPost.content);
+            expect(wrapper.find("p").text()).toBe(mockPost.content);
+        });
+        test("snapshot", () => {
+            const mockPost = {
+                content: "I am learning to test React components"
+            };
+            const component = renderer.create(<Content post={mockPost}/>);
+            const tree = component.toJSON();
+            console.log(tree);
+            expect(tree).toMatchSnapshot();
+        });
     });
 });
