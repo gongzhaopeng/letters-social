83929e8ce73912c79c18e03327bd315f61ee3698
gong zhaopeng <gongzhaopeng@gongs-MacBook-Air.local>
2019-10-02 19:54:57 +0800

    ...

--- src/shared/crash.js
+++ src/shared/crash.js
@@ -1,3 +1,3 @@
 if (process.env.NODE_ENV === 'production') {
-    window.Raven.config('https://23f0e00b78a24ac88450c8261b59ed7c@sentry.io/212515').install();
+    window.Raven.config('https://3578aee2ae8b44ad9e2cd67a98e19678@sentry.io/1768058').install();
 }
