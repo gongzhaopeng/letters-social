e086179f4a136db1e44fbe2df7bc8c30d6d9fed8
gong zhaopeng <gongzhaopeng@gongs-MacBook-Air.local>
2019-10-07 22:54:04 +0800

    ...

---
+++ src/reducers/loading.js
@@ -0,0 +1,16 @@
+/**
+ * Created by gongzhaopeng on 2019/10/7.
+ */
+import initialState from '../constants/initialState';
+import * as types from '../constants/types';
+
+export function loading(state = initialState.loading, action) {
+    switch (action.type) {
+        case types.app.LOADING:
+            return true;
+        case types.app.LOADED:
+            return false;
+        default:
+            return state;
+    }
+}
